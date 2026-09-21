c83b272ad51af67b889d665da08d3d41437fea60
khantalhaahmad <khantalhaahmad978@gmail.com>
2026-09-22 05:13:20 +0530

    Add Java basics practice programs

---
+++ practise2.java
@@ -0,0 +1,8 @@
+public class practise2 {
+    public static void main (String[] args) {
+        int age = 24;
+        String name = "Talha Ahmed Khan";
+        System.out.println("My name is: " + name + " and I am " + age + " years old.");
+    }
+    
+}
---
+++ practise3.java
@@ -0,0 +1,15 @@
+public class practise3 {
+    public static void main(String[] args) {
+       String name = "Talha Ahmed Khan";
+       int age = 22;
+       char grade = 'A';
+       double salary = 25000.5;
+       boolean JavaLearner = true;
+       System.out.println("My name is: " + name + " and I am " + age + " years old.");
+       System.out.println("My Grade is: " + grade);
+       System.out.println("My Salary is: " + salary);
+       System.out.println("Am I a Java Learner? " + JavaLearner);
+    
+    }
+    
+}
