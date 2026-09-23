05a085f0052b3a2632a962f660ece1c6b5179523
khantalhaahmad <khantalhaahmad978@gmail.com>
2026-09-23 12:19:53 +0530

    Add comparison operators practice

---
+++ practise9.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class practise9 {
+    public static void main(String[] args ) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your number");
+        int a = sc.nextInt();
+
+        System.out.println("Enter your second number");
+        int b = sc.nextInt();
+
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+
+
+    
+}
+
+}
