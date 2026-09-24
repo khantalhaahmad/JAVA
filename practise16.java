397f5b191799b2b6fe59d494057a19e4a4d4df25
khantalhaahmad <khantalhaahmad978@gmail.com>
2026-09-24 13:17:38 +0530

    Add positive negative and even odd practice

---
+++ practise16.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class practise16 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your number");
+        int a = sc.nextInt();
+
+        if(a>0){
+            System.out.println("Number is positive");
+        }
+        else if (a<0){
+            System.out.println("Number is negative");
+        }
+        else{
+            System.out.println("Number is zero");
+        }
+
+
+        if (a%2==0){
+            System.out.println("Number is Even");
+        }
+        else{
+            System.out.println("Number is Odd");
+        }
+        
+    }
+    
+}
