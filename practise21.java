6652c2a6c9ed34b2d8cfb813a35aa8c2bcfde4ce
khantalhaahmad <khantalhaahmad978@gmail.com>
2026-09-25 21:32:07 +0530

    added more question

---
+++ practise21.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class practise21 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your 1st number");
+        int a = sc.nextInt();
+
+        System.out.println("Enter your 2nd number");
+        int b = sc.nextInt();
+
+        
+
+    }
+    
+}
