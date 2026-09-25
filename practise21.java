24de8c102358e863a3f3db0ceb5056075607f7a4
khantalhaahmad <khantalhaahmad978@gmail.com>
2026-09-25 21:40:06 +0530

    Added Calculator Problem

--- practise21.java
+++ practise21.java
@@ -9,7 +9,28 @@ public class practise21 {
         System.out.println("Enter your 2nd number");
         int b = sc.nextInt();
 
-        
+        System.out.println("Enter your operator");
+
+        char operator= sc.next().charAt(0);
+
+        System.out.println("Selected operator is " + operator);
+
+        switch(operator){
+            case '+':
+                System.out.println(a+b);
+                break;
+            case '-':
+                System.out.println(a-b);
+                break;
+            case '*':
+                System.out.println(a*b);
+                break;
+            case '/':
+                System.out.println(a/b);
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
 
     }
     
